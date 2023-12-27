2f5bc497ba9108dd525b6e1590a709d5aaf533c0
woxnet <woxnet@gmail.com>
2023-12-27 16:00:39 +0300

    8_6

--- main.cpp
+++ main.cpp
@@ -1,177 +1,91 @@
-#include <algorithm>
 #include <iostream>
-#include <set>
-#include <string>
-#include <utility>
-#include <vector>
 #include <map>
-#include <cmath>
+#include <string>
+#include <tuple>
+#include <vector>
 
 using namespace std;
 
-const int MAX_RESULT_DOCUMENT_COUNT = 5;
-
-string ReadLine() {
-    string s;
-    getline(cin, s);
-    return s;
-}
-
-int ReadLineWithNumber() {
-    int result = 0;
-    cin >> result;
-    ReadLine();
-    return result;
-}
-
-vector<string> SplitIntoWords(const string& text) {
-    vector<string> words;
-    string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        }
-        else {
-            word += c;
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-
-    return words;
-}
-
-struct Document {
-    int id;
-    double relevance;
+// Перечислимый тип для статуса задачи
+enum class TaskStatus {
+    NEW,          // новая
+    IN_PROGRESS,  // в разработке
+    TESTING,      // на тестировании
+    DONE          // завершена
 };
 
-struct Query {
-    set<string> plusword;
-    set<string> minusword;
-};
+// Объявляем тип-синоним для map<TaskStatus, int>,
+// позволяющего хранить количество задач каждого статуса
+using TasksInfo = map<TaskStatus, int>;
 
-class SearchServer {
+class TeamTasks {
 public:
-    void SetStopWords(const string& text) {
-        for (const string& word : SplitIntoWords(text)) {
-            stop_words_.insert(word);
-        }
+    // Получить статистику по статусам задач конкретного разработчика
+    const TasksInfo& GetPersonTasksInfo(const string& person) const {
+
+        return persons.at(person);
     }
 
-    void AddDocument(int document_id, const string& document) {
-        const vector<string> words = SplitIntoWordsNoStop(document);
-        double TF = 1.0 / words.size();
-        ++document_count_;
-
-        for (auto& word : words) {  
-            word_to_document_freqs_[word][document_id] += TF;
-        }
+    // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+    void AddNewTask(const string& person) {
+        ++persons[person][TaskStatus::NEW];
     }
 
-    vector<Document> FindTopDocuments(const string& raw_query) const {
-        Query query = ParseQuery(raw_query);
-        auto matched_documents = FindAllDocuments(query);
+    // Обновить статусы по данному количеству задач конкретного разработчика,
+    // подробности см. ниже
+    tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string& person, int task_count) {
+        tuple<TasksInfo, TasksInfo> result;
 
-        sort(matched_documents.begin(), matched_documents.end(),
-            [](const Document& lhs, const Document& rhs) {
-                return lhs.relevance > rhs.relevance;
-            });
-        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
-            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        TasksInfo touched;
+        TasksInfo untouched;
+
+        if (persons.count(person) == 0) {
+            return result;
         }
-        return matched_documents;
+        for (int i = 0; i < task_count; ++i) {
+
+        }
+
+        return tie(touched, untouched);
+
     }
 
 private:
-
-    map<string, map<int, double>> word_to_document_freqs_;
-
-    set<string> stop_words_;
-
-    int document_count_ = 0;
-
-    bool IsStopWord(const string& word) const {
-        return stop_words_.count(word) > 0;
-    }
-
-    vector<string> SplitIntoWordsNoStop(const string& text) const {
-        vector<string> words;
-        for (const string& word : SplitIntoWords(text)) {
-            if (!IsStopWord(word)) {
-                words.push_back(word);
-            }
-        }
-        return words;
-    }
-
-    vector<Document> FindAllDocuments(const Query query) const {
-        vector<Document> matched_documents;
-        map<int, double> doc_for_relevance;    
-        int count;
-        double IDF;
-
-        for (const string& word : query.plusword) {
-            if (word_to_document_freqs_.count(word)) {
-                count = word_to_document_freqs_.at(word).size();
-                IDF = log(document_count_ / static_cast<double>(count));
-                for (auto& [key, TF] : word_to_document_freqs_.at(word)) {
-                    doc_for_relevance[key] += IDF * TF;
-                }
-            }
-        }
-
-        for (const string& word : query.minusword) {
-            if (word_to_document_freqs_.count(word)) {
-                for (const auto& [key, value] : word_to_document_freqs_.at(word)) {
-                    doc_for_relevance.erase(key);
-                }
-            }
-        }
-
-        for (auto& [key, value] : doc_for_relevance) {
-            matched_documents.push_back({ key, value });
-        }
-        return matched_documents;
-    }
-
-    Query ParseQuery(const string& text) const {
-        Query temp;
-        for (const string& word : SplitIntoWords(text)) {
-            if (word[0] == '-') {
-                if (!IsStopWord(word)) {
-                    temp.minusword.insert(word.substr(1));
-                }
-            }
-            else
-                temp.plusword.insert(word);
-        }
-        return temp;
-    }
+    map<string, TasksInfo> persons;
 };
 
-SearchServer CreateSearchServer() {
-    SearchServer search_server;
-    search_server.SetStopWords(ReadLine());
-
-    const int document_count = ReadLineWithNumber();
-    for (int document_id = 0; document_id < document_count; ++document_id) {
-        search_server.AddDocument(document_id, ReadLine());
-    }
-
-    return search_server;
+// Принимаем словарь по значению, чтобы иметь возможность
+// обращаться к отсутствующим ключам с помощью [] и получать 0,
+// не меняя при этом исходный словарь.
+void PrintTasksInfo(TasksInfo tasks_info) {
+    cout << tasks_info[TaskStatus::NEW] << " new tasks"s
+        << ", "s << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress"s
+        << ", "s << tasks_info[TaskStatus::TESTING] << " tasks are being tested"s
+        << ", "s << tasks_info[TaskStatus::DONE] << " tasks are done"s << endl;
 }
 
 int main() {
-    const SearchServer search_server = CreateSearchServer();
-
-    const string query = ReadLine();
-    for (const auto& [document_id, relevance] : search_server.FindTopDocuments(query)) {
-        cout << "{ document_id = "s << document_id << ", "
-            << "relevance = "s << relevance << " }"s << endl;
+    TeamTasks tasks;
+    tasks.AddNewTask("Ilia"s);
+    for (int i = 0; i < 3; ++i) {
+        tasks.AddNewTask("Ivan"s);
     }
+    cout << "Ilia's tasks: "s;
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"s));
+    cout << "Ivan's tasks: "s;
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"s));
+
+    TasksInfo updated_tasks, untouched_tasks;
+
+    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    cout << "Updated Ivan's tasks: "s;
+    PrintTasksInfo(updated_tasks);
+    cout << "Untouched Ivan's tasks: "s;
+    PrintTasksInfo(untouched_tasks);
+
+    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    cout << "Updated Ivan's tasks: "s;
+    PrintTasksInfo(updated_tasks);
+    cout << "Untouched Ivan's tasks: "s;
+    PrintTasksInfo(untouched_tasks);
 }
