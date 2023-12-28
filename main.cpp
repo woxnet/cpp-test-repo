4a1b559f862b6978e4c7ce425ec0d3c4be5671fc
woxnet <woxnet@gmail.com>
2023-12-28 16:46:18 +0300

    new step

--- main.cpp
+++ main.cpp
@@ -20,6 +20,17 @@ enum class TaskStatus
 // позволяющего хранить количество задач каждого статуса
 using TasksInfo = map<TaskStatus, int>;
 
+// Принимаем словарь по значению, чтобы иметь возможность
+// обращаться к отсутствующим ключам с помощью [] и получать 0,
+// не меняя при этом исходный словарь.
+void PrintTasksInfo(TasksInfo tasks_info)
+{
+    cout << tasks_info[TaskStatus::NEW] << " new tasks"s
+         << ", "s << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress"s
+         << ", "s << tasks_info[TaskStatus::TESTING] << " tasks are being tested"s
+         << ", "s << tasks_info[TaskStatus::DONE] << " tasks are done"s << endl;
+}
+
 class TeamTasks
 {
 public:
@@ -29,7 +40,13 @@ public:
 
         return persons_.at(person);
     }
-
+    
+    void AddState (const string &person){
+        persons_.at(person)[TaskStatus::NEW] = 3;
+        persons_.at(person)[TaskStatus::IN_PROGRESS] = 2;
+        persons_.at(person)[TaskStatus::TESTING] = 4;
+        persons_.at(person)[TaskStatus::DONE] = 1;
+    }
     // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
     void AddNewTask(const string &person)
     {
@@ -44,49 +61,52 @@ public:
 
         TasksInfo touched;
         TasksInfo untouched = persons_.at(person);
-         
+        TasksInfo temp;
+
         for (int i = 0; i < task_count; ++i)
         {
-            TasksInfo temp = persons_.at(person);
+            temp = persons_.at(person);
             for (const auto &[taskStatus, count] : temp)
             {
-                if (count == 0)
+                if ((count <= 0) || (taskStatus == TaskStatus::DONE))
                 {
                     continue;
                 }
-                if (persons_.at(person)[taskStatus] >= 1)
-                {
-                    persons_.at(person)[taskStatus] -= 1;
-                    untouched[taskStatus] -= 1;
-                    if (untouched[taskStatus] <= 0)
-                    {
-                        untouched.erase(taskStatus);
-                    }
+                cout << "Touched "s << endl;
+                PrintTasksInfo(touched);
 
-                    touched[static_cast<TaskStatus>(static_cast<int>(taskStatus) + 1)] += 1;
-                    persons_.at(person)[static_cast<TaskStatus>(static_cast<int>(taskStatus) + 1)] += 1;
-                    break;
+                cout << "Person "s << endl;
+                PrintTasksInfo(temp);
+
+                --touched[taskStatus];
+                if ((touched[taskStatus] <= 0))
+                {
+                    touched.erase(taskStatus);
                 }
+                
+                ++touched[static_cast<TaskStatus>(static_cast<int>(taskStatus) + 1)];
+
+                --persons_.at(person)[taskStatus];
+                ++persons_.at(person)[static_cast<TaskStatus>(static_cast<int>(taskStatus) + 1)];
+                --untouched[taskStatus];
+
+                if (untouched[taskStatus] < 1)
+                {
+                    untouched.erase(taskStatus);
+                }
+                break;
             }
         }
-
+        untouched.erase(TaskStatus::DONE);
         return tie(touched, untouched);
     }
 
 private:
-    map<string, TasksInfo> persons_;
+    map<string, TasksInfo>
+        persons_;
 };
 
-// Принимаем словарь по значению, чтобы иметь возможность
-// обращаться к отсутствующим ключам с помощью [] и получать 0,
-// не меняя при этом исходный словарь.
-void PrintTasksInfo(TasksInfo tasks_info)
-{
-    cout << tasks_info[TaskStatus::NEW] << " new tasks"s
-         << ", "s << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress"s
-         << ", "s << tasks_info[TaskStatus::TESTING] << " tasks are being tested"s
-         << ", "s << tasks_info[TaskStatus::DONE] << " tasks are done"s << endl;
-}
+
 
 int main()
 {
@@ -96,22 +116,27 @@ int main()
     {
         tasks.AddNewTask("Ivan"s);
     }
-    cout << "Ilia's tasks: "s;
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"s));
+
+    tasks.AddState("Ivan"s);
+
+    // cout << "Ilia's tasks: "s;
+    // PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"s));
     cout << "Ivan's tasks: "s;
     PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"s));
 
     TasksInfo updated_tasks, untouched_tasks;
 
-    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 40);
     cout << "Updated Ivan's tasks: "s;
     PrintTasksInfo(updated_tasks);
     cout << "Untouched Ivan's tasks: "s;
     PrintTasksInfo(untouched_tasks);
 
-    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
-    cout << "Updated Ivan's tasks: "s;
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: "s;
-    PrintTasksInfo(untouched_tasks);
+    cout << "---------------------------------------------------------------------------------"s << endl;
+
+    // tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    // cout << "Updated Ivan's tasks: "s;
+    // PrintTasksInfo(updated_tasks);
+    // cout << "Untouched Ivan's tasks: "s;
+    // PrintTasksInfo(untouched_tasks);
 }
